95ddea6234d0e0d2e63dd898a7f1fe7db7d2c6d2
Никита Запорожец <nikitazaporohzets@MacBook-Air.local>
2026-01-03 12:09:33 +0300

    Fix indentation on line 2552

--- moviebot.py
+++ moviebot.py
@@ -2549,8 +2549,8 @@ def premiere_detail_handler(call):
         if director_str != '—':
             text += f"🎥 Режиссёр: {director_str}\n"
         if countries != '—':
-        text += f"🌍 {countries}\n"
-        text += f"\n{description}\n\n"
+            text += f"🌍 {countries}
+"        text += f"\n{description}\n\n"
         text += f"🎭 {genres}\n"
         
         markup = InlineKeyboardMarkup(row_width=1)
